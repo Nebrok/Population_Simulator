4fa0dbdaaff75c03a67dffbe59a3f6770a2e8e82
Rooster202 <42103451+Rooster202@users.noreply.github.com>
2018-11-05 09:10:34 +1000

    Scrolling Graph added

--- class_Graph.py
+++ class_Graph.py
@@ -1,3 +1,4 @@
+import main
 import pygame
 import random
 import math
@@ -13,15 +14,24 @@ class Graph():
     def __init__(self, res, screen):
         self.res = res
         self.screen = screen
+        self.last_iter = 0
 
     def draw(self, lisst):
-        prev_pos = (self.res[0]/2, self.res[1])
+        prev_pos = ((self.res[0]/2)-self.last_iter, self.res[1])
         i = 0
         for population in lisst: 
-            cur_pos = (self.res[0]/2+i/8, self.res[1]-population/600)
+            cur_pos = ((self.res[0]/2+i/8)-self.last_iter, self.res[1]-population/600)
             pygame.draw.line(self.screen, BLACK, prev_pos, cur_pos, 2)
             prev_pos = cur_pos
             i += 1
+        if cur_pos[0] > self.res[0]:
+            self.last_iter += cur_pos[0]-self.res[0]
+            print(self.last_iter)
+
+
+
+if __name__ == '__main__':
+    main.main()
             
             
         
